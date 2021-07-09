dc18e3f2cb8a1d9ab9c834512de32f72ac556125
Gui Barros <wingui3@gmail.com>
2021-07-09 02:23:09 -0300

    💄 Add max-width to body

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -1,10 +1,14 @@
+import "./style.css";
 import CardGame from "../../components/CardGame";
 
 function BoardGame (amountCards) {
     const $htmlCardGame = CardGame();
-    const $htmlBoardGame = $htmlCardGame.repeat(amountCards); 
+    const $htmlContent = $htmlCardGame.repeat(amountCards); 
 
-    return $htmlBoardGame; 
+return `
+    <section class="board-game">
+    ${$htmlContent}
+    </section>`; 
 }
 
 export default BoardGame;
