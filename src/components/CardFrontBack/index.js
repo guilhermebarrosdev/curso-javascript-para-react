c50ea4a430cf14c524fea1dde61f9323fe5b1785
Gui Barros <wingui3@gmail.com>
2021-07-14 10:34:33 -0300

    ✨ Add the CardGame with Javascript icon

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -4,7 +4,7 @@ function CardFrontBack() {
     return /*html*/`
         <article class="card-front-back">
             ${CardGame()}
-            ${CardGame()}
+            ${CardGame("logo-javascript", "logo do javascript")}
         </article>
     `
 }
